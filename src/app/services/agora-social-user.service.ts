446ede68d60c1a69aed252feb02e5e96d61c9165
designkenal <alwiscoding@gmail.com>
2018-11-13 16:09:39 +0800

    WIP

---
+++ src/app/services/agora-social-user.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class AgoraSocialUserService{
+
+  getIsSocialUser(): String {
+    return window.localStorage['isSocialUser'];
+  }
+
+  saveIsSocialUser(value: String) {
+    window.localStorage['isSocialUser'] = value;
+  }
+
+  destroyIsSocialUser() {
+    window.localStorage.removeItem('isSocialUser');
+  }
+}
