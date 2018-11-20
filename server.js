dbcf395e6c15748989d04e846c50ab3c42e929b8
icemc <abandaludovic500@gmail.com>
2018-11-20 10:53:34 +0100

    enforce https

--- server.js
+++ server.js
@@ -4,6 +4,28 @@ const express = require('express');
 
 const app = express();
 
+// If an incoming request uses
+// a protocol other than HTTPS,
+// redirect that request to the
+// same url but with HTTPS
+
+const forceSSL = function () {
+    return function (req, res, next) {
+        if (req.headers['x-forwarded-proto'] !== 'https') {
+            return res.redirect(
+                ['https://', req.get('Host'), req.url].join('')
+            );
+        }
+        next();
+    }
+}
+
+// Instruct the app
+// to use the forceSSL
+// middleware
+
+app.use(forceSSL());
+
 const path = require('path');
 
 // Serve only the static files form the dist directory
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  API_URL: 'http://agora-rest-api.herokuapp.com/api/v1'
+  API_URL: 'https://agora-rest-api.herokuapp.com/api/v1'
 };
