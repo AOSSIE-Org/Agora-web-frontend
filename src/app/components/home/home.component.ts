1c50f7a75b27c3c82cff8a2b289bfed8da7d6167
war-turtle <kartikyadav71199@gmail.com>
2019-03-03 17:56:58 +0530

    animation for home component

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../../services/user.service';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  animations: [
+    trigger('fadeInFirst', [
+      state('void', style({
+        opacity: 0,
+        transform: 'translateY(50px)'
+      })),
+      transition('void => *', animate('1000ms'))
+    ]),
+    trigger('fadeInSecond', [
+      state('void', style({
+        opacity: 0,
+        transform: 'translateY(50px)'
+      })),
+      transition('void => *', animate('1000ms 1000ms'))
+    ]),
+    trigger('fadeInThird', [
+      state('void', style({
+        opacity: 0,
+        transform: 'translateY(50px)'
+      })),
+      transition('void => *', animate('1000ms 2000ms'))
+    ])
+  ]
 })
 export class HomeComponent implements OnInit {
 
